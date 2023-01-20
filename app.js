7d1969d813152893e41004167bef0d03d1830312
natty-7 <nati7fekadu@gmail.com>
2023-01-20 11:02:05 +0300

    pharmacist version-1

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ const addDrug = () => {
 addDrug();
 const app = express();
 app.use((req, res, next) => {
-  console.log(req.path);
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
